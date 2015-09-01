1f24945c580c7f790c6ad495cde858fc560a5d45
cherylafitz <cherylafitzgerald@gmail.com>
2015-09-01 16:28:00 -0700

    created api using RESTful routing

---
+++ api/controllers/PersonController.js
@@ -0,0 +1,23 @@
+/**
+ * PersonController
+ *
+ * @description :: Server-side logic for managing people
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+  addCatbyName: function(req,res){
+    Person.findOne(req.params.id).then(function(person){
+      Category.findOrCreate({where: {name:req.body.name}}).then(function(category){
+        person.categories.add(category);
+        person.save(function(person){
+          console.log(person)
+          res.send(person);
+        });
+      });
+    });
+  }
+
+};
+
